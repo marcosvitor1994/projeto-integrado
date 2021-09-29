c2fbc42b39621c6dc8a0f982850e81b78ac54f47
Marcos Vitor <marcos.vitor@iesb.edu.br>
2021-09-28 22:29:41 -0300

    pronto

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
